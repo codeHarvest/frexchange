264fe65baa66b3dedef444c504a3f16af1f379c4
codeHarvest <madhusudanbhardwaj94@gmail.com>
2019-12-15 16:35:56 +0530

    login and signup react components

--- client/src/Components/Login/Login.component.js
+++ client/src/Components/Login/Login.component.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React,{ Component } from 'react'
 
-export default function Login() {
-    return (
-        <div className="login">
-            <form className="login-form">
-                login
-            </form>
-        </div>
-    )
+export default class Login extends Component{
+    render(){
+        return (
+            <div className="login">
+                <form className="form--login">
+                    <label className="label--email element">email:</label>
+                    <input className="email element" name="email" type="email" placeholder="email" value="" required/>
+                    <label className="label--password element">password:</label>
+                    <input className="password element" name="password" type="password" placeholder="password" value="" required />
+                    <button type="button" className="btn--login btn">Login</button>
+                    <button type="button" className="btn--google btn">Google login</button>   
+                </form>
+            </div>
+        )
+    }
 }
--- client/src/Components/Signup/Signup.component.js
+++ client/src/Components/Signup/Signup.component.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{ Component } from 'react'
 
-export default function Signup() {
-    return (
-        <div className="signup">
-         <form className="signup-form">
-            signup
-         </form>   
-        </div>
-    )
+export default class Signup extends Component{
+
+    render(){
+        return (
+            <div className="signup">
+             <form className="form--signup">
+                <label className="label--username element">username:</label>
+                <input name="displayName" className="username element" type="text" placeholder="username" value="" required />
+                <label className="label--email element">email:</label>
+                <input name="email" className="username element" type="email" placeholder="email" value="" required/>
+                <label className="label--password element">password:</label>
+                <input name="password" className="username element" type="password" placeholder="password" value="" required />
+                <label className="label--confirmpassword element">Confirm Password:</label>
+                <input name="confirmPassword" className="confirm_password element" type="password" placeholder="confirm password" value="" required />
+                <button type="button" className="btn--login btn">Register</button>
+             </form>  
+            </div>
+        )
+    }
 }
