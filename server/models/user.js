02886b4dd3d130df8eac16dcdba74b15f61ce8ce
codeHarvest <madhusudanbhardwaj94@gmail.com>
2019-12-16 22:49:51 +0530

    hashing password on register

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
-// const bcrypt = require('bcrypt');
-// const saltRounds = 10;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = new schema({
     name: {
@@ -20,6 +20,32 @@ const userSchema = new schema({
         minLength: 6
     }
 });
+
+userSchema.pre('save', function(next){
+    var password = this.password;
+    var self = this;
+    console.log(this, this.password, this.isModified(this.password, 'password'));
+
+    if(this.isModified(this.password)) return next();
+
+    bcrypt.genSalt(saltRounds, function(err, salt){
+        bcrypt.hash(password, salt, function(err, hash){
+            self.password = hash;
+            next();
+        })
+    })
+});
+
+// userSchema.methods.comparePassword = function(userPassword, cb){
+//     bcrypt.compare(userPassword, this.password, function(err, isMatch){
+//         console.log(isMatch,'isMatch');
+//         if(err){
+//             return cb(err, false);
+//         }
+//         return cb(null,isMatch);
+//     })
+// }
+
 
 const user = mongoose.model('User', userSchema);
 
