4c88e6c3ba2b6222d7c8bd079209c4db79cbefbc
codeHarvest <madhusudanbhardwaj94@gmail.com>
2020-01-04 23:00:58 +0530

    fixing bcrypt password hashing

--- server/models/user.js
+++ server/models/user.js
@@ -7,8 +7,8 @@ const userSchema = new schema({
     name: {
         type: String,
         required: [true, 'cannot be blank'],
-        minlength: 4,
-        maxlength: 28
+        minLength: 4,
+        maxLength: 28
     },
     email: {
         type: String,
@@ -24,27 +24,26 @@ const userSchema = new schema({
 userSchema.pre('save', function(next){
     var password = this.password;
     var self = this;
-    console.log(this, this.password, this.isModified(this.password, 'password'));
-
+  
     if(this.isModified(this.password)) return next();
-
-    bcrypt.genSalt(saltRounds, function(err, salt){
-        bcrypt.hash(password, salt, function(err, hash){
-            self.password = hash;
-            next();
-        })
-    })
-});
-
-userSchema.methods.comparePassword = function(userPassword, cb){
-    bcrypt.compare(userPassword, this.password, function(err, isMatch){
-        console.log(isMatch,'isMatch');
-        if(err){
-            return cb(err, false);
-        }
-        return cb(null, isMatch);
-    })
-}
+  
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      bcrypt.hash(password, salt, function(err, hash) {
+          self.password = hash;
+          next();
+        });
+      });
+  });
+  
+  userSchema.methods.comparePassword = function(userPassword, cb){
+    bcrypt.compare(userPassword, this.password, function(err,isMatch){
+      if(err) {
+        return cb(err, false);
+      }
+      return cb(null, isMatch);
+    });
+  }
+    
 
 
 const user = mongoose.model('User', userSchema);
--- server/modules/passport.js
+++ server/modules/passport.js
@@ -6,32 +6,33 @@ const User = require('../models/user');
 
 
 module.exports = function(passport){
-
     passport.serializeUser(function(user,done){
-        console.log('serializeUser');
-        done(null,user._id);
-    });
-
-    passport.deserializeUser(function (id,done){
-        console.log('deserializeuser');
-        User.findById(id, function(err,user){
-            done(err,user);
+        // console.log('serializeUser');
+        done(null, user._id);
+      });
+    
+      passport.deserializeUser(function (id, done){
+        // console.log('deserializeUser');
+        User.findById(id, function(err, user){
+          done(err,user);
         })
-    });
-
-    passport.use(new localStrategy({ usernameField : 'email'}, function(email, password, done){
-        User.findOne({ email : email }, function(err,user){
-            user.comparePassword(password,(err, isMatch) => {
-                if(err){
-                    return done(err);  
-                };
-                if(!isMatch){
-                    return done(null,false);
-                }
-                return done(null,user);
+      });
+    
+      passport.use(new localStrategy({ usernameField: 'email' },function(email, password, done) {
+        User.findOne({email: email},function(err,user){
+          user.comparePassword(password,(err, isMatch) => {
+            if(err){
+              return done(err)
+            };
+            if(!isMatch){
+              return done(null,false);
+            }
+            // console.log(user,"in passport.js")
+              return done(null,user);
             })
-        })
-    }))
+          })      
+        }
+      ))
 
     // passport.use(new GoogleStrategy({
     //     clientID : process.env.clientID,
